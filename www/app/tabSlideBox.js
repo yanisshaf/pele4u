afdc07f767ffb29915a9a0165f58be123ea526ee
User <golanh@ubuntu.ubuntu-domain>
2017-08-30 17:00:21 +0300

    fix white page on tab-slide-box

--- www/app/tabSlideBox.js
+++ www/app/tabSlideBox.js
@@ -28,29 +28,30 @@ function SimplePubSub() {
 };
 
 angular.module('tabSlideBox', [])
-  .directive('onFinishRender', function ($timeout) {
+  .directive('onFinishRender', function($timeout) {
     return {
       restrict: 'A',
-      link: function (scope, element, attr) {
+      link: function(scope, element, attr) {
         if (scope.$last === true) {
-          $timeout(function () {
+          $timeout(function() {
             scope.$emit('ngRepeatFinished');
           });
         }
       }
     }
   })
-  .directive('tabSlideBox', [ '$timeout', '$window', '$ionicSlideBoxDelegate', '$ionicScrollDelegate',
+  .directive('tabSlideBox', ['$timeout', '$window', '$ionicSlideBoxDelegate', '$ionicScrollDelegate',
     function($timeout, $window, $ionicSlideBoxDelegate, $ionicScrollDelegate) {
       'use strict';
 
       return {
-        restrict : 'A, E, C',
-        link : function(scope, element, attrs, ngModel) {
-
-          var ta = element[0], $ta = element;
+        restrict: 'A, E, C',
+        link: function(scope, element, attrs, ngModel) {
+          $ionicSlideBoxDelegate.stop()
+          var ta = element[0],
+            $ta = element;
           $ta.addClass("tabbed-slidebox");
-          if(attrs.tabsPosition === "bottom"){
+          if (attrs.tabsPosition === "bottom") {
             $ta.addClass("btm");
           }
 
@@ -58,38 +59,41 @@ angular.module('tabSlideBox', [])
           var handle = ta.querySelector('.slider').getAttribute('delegate-handle');
 
           var ionicSlideBoxDelegate = $ionicSlideBoxDelegate;
-          if(handle){
+          if (handle) {
             ionicSlideBoxDelegate = ionicSlideBoxDelegate.$getByHandle(handle);
           }
 
           var ionicScrollDelegate = $ionicScrollDelegate;
-          if(handle){
+          if (handle) {
             ionicScrollDelegate = ionicScrollDelegate.$getByHandle(handle);
           }
 
-          function renderScrollableTabs(){
-            var iconsDiv = angular.element(ta.querySelector(".tsb-icons")), icons = iconsDiv.find("a"), wrap = iconsDiv[0].querySelector(".tsb-ic-wrp"), totalTabs = icons.length;
+          function renderScrollableTabs() {
+            var iconsDiv = angular.element(ta.querySelector(".tsb-icons")),
+              icons = iconsDiv.find("a"),
+              wrap = iconsDiv[0].querySelector(".tsb-ic-wrp"),
+              totalTabs = icons.length;
             var scrollDiv = wrap.querySelector(".scroll");
 
-            angular.forEach(icons, function(value, key){
+            angular.forEach(icons, function(value, key) {
               var a = angular.element(value);
-              a.on('click', function(){
+              a.on('click', function() {
                 ionicSlideBoxDelegate.slide(key);
               });
 
-              if(a.attr('icon-off')) {
+              if (a.attr('icon-off')) {
                 a.attr("class", a.attr('icon-off'));
               }
             });
 
             var initialIndex = attrs.tab;
             //Initializing the middle tab
-            if(typeof attrs.tab === 'undefined' || (totalTabs <= initialIndex) || initialIndex < 0){
-              initialIndex = Math.floor(icons.length/2);
+            if (typeof attrs.tab === 'undefined' || (totalTabs <= initialIndex) || initialIndex < 0) {
+              initialIndex = Math.floor(icons.length / 2);
             }
 
             //If initial element is 0, set position of the tab to 0th tab
-            if(initialIndex == 0){
+            if (initialIndex == 0) {
               setPosition(0);
             }
 
@@ -97,55 +101,62 @@ angular.module('tabSlideBox', [])
               ionicSlideBoxDelegate.slide(initialIndex);
             }, 0);
           }
-          function setPosition(index){
-            var iconsDiv = angular.element(ta.querySelector(".tsb-icons")), icons = iconsDiv.find("a"), wrap = iconsDiv[0].querySelector(".tsb-ic-wrp"), totalTabs = icons.length;
+
+          function setPosition(index) {
+            var iconsDiv = angular.element(ta.querySelector(".tsb-icons")),
+              icons = iconsDiv.find("a"),
+              wrap = iconsDiv[0].querySelector(".tsb-ic-wrp"),
+              totalTabs = icons.length;
             var scrollDiv = wrap.querySelector(".scroll");
 
-            var middle = iconsDiv[0].offsetWidth/2;
+            var middle = iconsDiv[0].offsetWidth / 2;
             var curEl = angular.element(icons[index]);
             var prvEl = angular.element(iconsDiv[0].querySelector(".active"));
-            if(curEl && curEl.length){
-              var curElWidth = curEl[0].offsetWidth, curElLeft = curEl[0].offsetLeft;
+            if (curEl && curEl.length) {
+              var curElWidth = curEl[0].offsetWidth,
+                curElLeft = curEl[0].offsetLeft;
 
-              if(prvEl.attr('icon-off')) {
+              if (prvEl.attr('icon-off')) {
                 prvEl.attr("class", prvEl.attr('icon-off'));
-              }else{
+              } else {
                 prvEl.removeClass("active");
-              } 
-              if(curEl.attr('icon-on')) {
+              }
+              if (curEl.attr('icon-on')) {
                 curEl.attr("class", curEl.attr('icon-on'));
               }
               curEl.addClass("active");
 
-              var leftStr = (middle  - (curElLeft) -  curElWidth/2 + 5);
+              var leftStr = (middle - (curElLeft) - curElWidth / 2 + 5);
               //If tabs are not scrollable
-              if(!scrollDiv){
-                var leftStr = (middle  - (curElLeft) -  curElWidth/2 + 5) + "px";
-                wrap.style.webkitTransform =  "translate3d("+leftStr+",0,0)" ;
-              }else{
+              if (!scrollDiv) {
+                var leftStr = (middle - (curElLeft) - curElWidth / 2 + 5) + "px";
+                wrap.style.webkitTransform = "translate3d(" + leftStr + ",0,0)";
+              } else {
                 //If scrollable tabs
                 var wrapWidth = wrap.offsetWidth;
                 var currentX = Math.abs(getX(scrollDiv.style.webkitTransform));
                 var leftOffset = 100;
                 var elementOffset = 40;
                 //If tabs are reaching right end or left end
-                if(((currentX + wrapWidth) < (curElLeft + curElWidth + elementOffset)) || (currentX > (curElLeft - leftOffset))){
-                  if(leftStr > 0){
+                if (((currentX + wrapWidth) < (curElLeft + curElWidth + elementOffset)) || (currentX > (curElLeft - leftOffset))) {
+                  if (leftStr > 0) {
                     leftStr = 0;
                   }
                   //Use this scrollTo, so when scrolling tab manually will not flicker
+
                   ionicScrollDelegate.scrollTo(Math.abs(leftStr), 0, true);
                 }
               }
             }
           };
+
           function getX(matrix) {
-            matrix = matrix.replace("translate3d(","");
-            matrix = matrix.replace("translate(","");
+            matrix = matrix.replace("translate3d(", "");
+            matrix = matrix.replace("translate(", "");
             return (parseInt(matrix));
           }
           var events = scope.events;
-          events.on('slideChange', function(data){
+          events.on('slideChange', function(data) {
             setPosition(data.index);
           });
           events.on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
@@ -154,16 +165,34 @@ angular.module('tabSlideBox', [])
 
           renderScrollableTabs();
         },
-        controller : function($scope, $attrs, $element) {
+        controller: function($scope, $attrs, $element) {
           $scope.events = new SimplePubSub();
 
-          $scope.slideHasChanged = function(index){
-            $scope.events.trigger("slideChange", {"index" : index});
-            $timeout(function(){if($scope.onSlideMove) $scope.onSlideMove({"index" : eval(index)});},100);
+          $scope.disable = function(event) {
+            $ionicSlideBoxDelegate.enableSlide(false);
+          }
+
+          $scope.enable = function(event) {
+            $ionicSlideBoxDelegate.enableSlide(true);
+          }
+
+          $scope.slideHasChanged = function(index) {
+
+            $scope.events.trigger("slideChange", {
+              "index": index
+            });
+            $timeout(function() {
+              if ($scope.onSlideMove) $scope.onSlideMove({
+                "index": eval(index)
+              });
+            }, 100);
           };
 
           $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-            $scope.events.trigger("ngRepeatFinished", {"event" : ngRepeatFinishedEvent});
+
+            $scope.events.trigger("ngRepeatFinished", {
+              "event": ngRepeatFinishedEvent
+            });
           });
         }
       };
