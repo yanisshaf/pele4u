a7c3cc994077539695bde3a01b56c72b3f90f18d
User <golanh@ubuntu.ubuntu-domain>
2017-09-24 18:07:50 +0300

    final lazyload - PD version

--- www_mini/app/messages.js
+++
@@ -1,7 +0,0 @@
-angular.module('pele.messages', [])
-  .constant("messages", {
-    no_cordova: "תכונה זאת  פעילה במכשיר בלבד",
-    mail: {
-      no_mail_account: "לא הוגדר חשבון אימייל במכשיר זה",
-    }
-  });
